79abed5830f3efc6e957135ac8a19f86ac84a674
Marcelo P. Hernandez <90793907+phmarcel0x@users.noreply.github.com>
2023-12-10 13:45:50 -0500

    updates selectionSort theory section

--- src/SortingAlgorithms/selectionSort.js
+++ src/SortingAlgorithms/selectionSort.js
@@ -1,13 +1,29 @@
 /* 
-Implementation of selection sort algorithm
-
+Implementation of SELECTION SORT algorithm:
 A simple and efficient sorting algorithm that works by repeatedly 
 selecting the smallest (or largest) element from the unsorted portion 
 of the list and moving it to the sorted portion of the list.
 
-ANALYSIS:
-TIME COMPLEXITY: Best Case O(n) | Average Case O(n^2) | Worst Case O(n^2) |
-SPACE COMPLEXITY: Worst Case O(1) |
+COMPLEXITY ANALYSIS:
+- Time Complexity: The time complexity of Selection Sort is O(N^2) as there are two nested loops:
+    - One loop to select an element of Array one by one = O(N)
+    - Another loop to compare that element with every other Array element = O(N)
+    - Therefore overall complexity = O(N) * O(N) = O(N*N) = O(N^2)
+
+- Auxiliary Space: O(1) as the only extra memory used is for temporary variables 
+    while swapping two values in Array. The selection sort never makes more than O(N) 
+    swaps and can be useful when memory writing is costly. 
+
+ADVANTAGES:
+- Simplicty, ease of understanding
+- Works well with small datasets
+- IN-SITU  as it does not require extra space
+
+DISADVANTAGES:
+- Time complexity of O(N^2) in average and worst case
+- Does not work well with large datasets
+- NOT STABLE as it does not preserve the relative order of items with equal keys
+
 */
 
 // Swap array elements
